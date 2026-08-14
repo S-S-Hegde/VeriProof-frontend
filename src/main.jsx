cb89e92f7f006306f2aef1c7750f7947324c10f7
hegde8055 <shridharhhegde@gmail.com>
2026-08-14 20:01:24 +0530

    Updated rendfer logic

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+// Automatically reload page if a deployment updates chunk hashes and a cached browser requests an old chunk
+window.addEventListener("vite:preloadError", () => {
+  window.location.reload();
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
